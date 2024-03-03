ce42369df64b8a1084d909e4d94402e55cca56ea
manigandan-d <manigandan220916@gmail.com>
2024-03-03 15:50:56 +0530

    updated

--- demo.c
+++ demo.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    char sentence[1000];
+    int *ptr = malloc(sizeof(int) * 5);
 
-    // Input the sentence
-    printf("Enter a sentence: ");
-    fgets(sentence, sizeof(sentence), stdin);
-
-    // Iterate through the characters of the sentence
-    int i = 0;
-    while (sentence[i] != '\0') {
-        // Skip leading spaces
-        while (sentence[i] == ' ') {
-            i++;
-        }
-
-        // Print the word
-        while (sentence[i] != ' ' && sentence[i] != '\0') {
-            printf("%c", sentence[i]);
-            i++;
-        }
-
-        // Move to the next word
-        printf("\n");
+    for(int i=0; i<5; i++) {
+        printf("%d ", *(ptr+i));
     }
+
+    free(ptr);
+
+    ptr = calloc(5, sizeof(int));
+
+    for(int i=0; i<5; i++) {
+        printf("%d ", *(ptr+i));
+    }
+
+    free(ptr);
 
     return 0;
 }
