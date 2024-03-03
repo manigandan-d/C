e2b64d716eeba64dbe41e0e471d672aa361968e4
manigandan-d <manigandan220916@gmail.com>
2024-03-03 15:50:19 +0530

    dynmaic allocation of 2D array

--- Tutorial/Derived Data Types/Array/Dynamic Memory Allocation in Array/array_2d.c
+++ Tutorial/Derived Data Types/Array/Dynamic Memory Allocation in Array/array_2d.c
@@ -1,46 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+int main() {
     int r, c, i, j;
 
-    printf("\nEnter the number of rows : ");
+    printf("Enter the number of rows: ");
     scanf("%d", &r);
 
-    printf("\nEnter the number of columns : ");
+    printf("Enter the number of columns: ");
     scanf("%d", &c);
 
-    int **a = malloc (r * sizeof(int *));
+    int **a = malloc(r * sizeof(int *));
     // int **a = (int **)malloc(r * sizeof(int *));
 
-    printf("\n");
-
-    for(i=0; i<r; i++)
-    {
+    for(i=0; i<r; i++) {
         a[i] = malloc(c * sizeof(int));
         // a[i] = (int *)malloc(c * sizeof(int));
     }
 
-    for(i=0; i<r; i++)
-    {
-        for(j=0; j<c; j++)
-        {
+    for(i=0; i<r; i++) {
+        for(j=0; j<c; j++) {
             scanf("%d", (*(a+i)+j));
         }
     }
 
-    for(i=0; i<r; i++)
-    {
-        printf("\n");
-
-        for(j=0; j<c; j++)
-        {
+    for(i=0; i<r; i++) {
+        for(j=0; j<c; j++) {
             printf("%d ", *(*(a+i)+j));
         }
     }
 
-    printf("\n");
+    for(i=0; i<r; i++) {
+        free(a[i]);
+    }
 
     free(a);
 
